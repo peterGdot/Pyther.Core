dd70ab70a451251d3c5e2c6fe777c99e16b30f51
Peter Grundmann <peter.grundmann.de@gmail.com>
2023-12-21 00:13:27 +0100

    Tests, Log objects, null checks, ...

--- Pyther.Core/Logging/Log.cs
+++ Pyther.Core/Logging/Log.cs
@@ -5,7 +5,7 @@ namespace Pyther.Core.Logging
     public static class Log
     {
         private static LogDataInstance? instance = null;
-        private static StringBuilder sb = new StringBuilder(5);
+        private static readonly StringBuilder sb = new(5);
 
         public static LogDataInstance Instance {
             get
@@ -53,7 +53,7 @@ namespace Pyther.Core.Logging
             {
                 if (type >= logger.MinLevel || type == LogLevel.Temp)
                 {
-                    logger.Log(type, String.Format(format, args));
+                    logger.Log(type, string.Format(format, args));
                 }
             }
         }
@@ -83,7 +83,7 @@ namespace Pyther.Core.Logging
         public static void WriteFormatLine(LogLevel type, string format, params object[] args)
         {
             DateTime now = DateTime.Now;
-            string data = String.Format(format + Environment.NewLine, args);
+            string data = string.Format(format + Environment.NewLine, args);
             foreach (ILogger logger in Instance.logger)
             {
                 if (logger.IsEnabled && (type >= logger.MinLevel || type == LogLevel.Temp))
@@ -110,11 +110,18 @@ namespace Pyther.Core.Logging
         public static void Warning(string str) => WriteLine(LogLevel.Warning, str);
         public static void Error(string str) => WriteLine(LogLevel.Error, str);
 
-        public static void TempF(string format, params object[] args) => WriteFormatLine(LogLevel.Temp, format, args);
-        public static void DebugF(string format, params object[] args) => WriteFormatLine(LogLevel.Debug, format, args);
-        public static void ProcessF(string format, params object[] args) => WriteFormatLine(LogLevel.Process, format, args);
-        public static void InfoF(string format, params object[] args) => WriteFormatLine(LogLevel.Info, format, args);
-        public static void WarningF(string format, params object[] args) => WriteFormatLine(LogLevel.Warning, format, args);
-        public static void ErrorF(string format, params object[] args) => WriteFormatLine(LogLevel.Error, format, args);
+        public static void Temp(object obj) => WriteLine(LogLevel.Temp, obj?.ToString() ?? "<NULL>");
+        public static void Debug(object obj) => WriteLine(LogLevel.Debug, obj?.ToString() ?? "<NULL>");
+        public static void Process(object obj) => WriteLine(LogLevel.Process, obj?.ToString() ?? "<NULL>");
+        public static void Info(object obj) => WriteLine(LogLevel.Info, obj?.ToString() ?? "<NULL>");
+        public static void Warnings(object obj) => WriteLine(LogLevel.Warning, obj?.ToString() ?? "<NULL>");
+        public static void Error(object obj) => WriteLine(LogLevel.Error, obj?.ToString() ?? "<NULL>");
+
+        public static void FTemp(string format, params object[] args) => WriteFormatLine(LogLevel.Temp, format, args);
+        public static void FDebug(string format, params object[] args) => WriteFormatLine(LogLevel.Debug, format, args);
+        public static void FProcess(string format, params object[] args) => WriteFormatLine(LogLevel.Process, format, args);
+        public static void FInfo(string format, params object[] args) => WriteFormatLine(LogLevel.Info, format, args);
+        public static void FWarning(string format, params object[] args) => WriteFormatLine(LogLevel.Warning, format, args);
+        public static void FError(string format, params object[] args) => WriteFormatLine(LogLevel.Error, format, args);
     }
 }
--- Tests/StringExtensionsTest.cs
+++ Tests/StringExtensionsTest.cs
@@ -1,10 +1,33 @@
 ﻿using Pyther.Core.Extensions;
+using System.Globalization;
+using System.Runtime.Versioning;
 
 namespace Tests;
 
 [TestClass]
 public class StringExtensionsTest
 {
+    [TestMethod]
+    public void TestManipulation()
+    {
+        Assert.AreEqual("abcabcabc", "abc".Repeat(3));
+        Assert.AreEqual("cba", "abc".Reverse());
+        Assert.AreEqual("Hello", "Hello World!".Clip(5));
+
+        Assert.AreEqual("This is a test.", "This <b>is a</b> test.".RemoveTags());
+        Assert.AreEqual("This is a test.", "This is </br>a test.".RemoveTags());
+        Assert.AreEqual("This test.", "This <!-- is a -->test.".RemoveHtmlComments());
+    }
+
+    [TestMethod]
+    public void TestTransform()
+    {
+        Assert.AreEqual("YWJj", "abc".Base64Encode());
+        Assert.AreEqual("abc", "YWJj".Base64Decode());
+        Assert.AreEqual("name", "Name".LowerFirst());
+        Assert.AreEqual("Name", "name".UpperFirst());
+    }
+
     [TestMethod]
     public void TestNamingPolicy()
     {
@@ -27,5 +50,51 @@ public class StringExtensionsTest
         Assert.AreEqual("my_class_name", "myClassName".CamelToSnakeCase());
         Assert.AreEqual("my-class-name", "myClassName".CamelToKebabCase());
         Assert.AreEqual("MyClassName", "myClassName".CamelToTitleCase());
+    }
+
+    [TestMethod]
+    public void TestParse()
+    {
+        Assert.AreEqual(123, "123".Parse<int>());
+        Assert.AreEqual(123.45, "123.45".Parse<double>());
+        Assert.AreEqual(123.45f, "123.45".Parse<float>());
+        Assert.AreEqual(true, "true".Parse<bool>());
+        Assert.AreEqual(false, "false".Parse<bool>());
+        Assert.AreEqual(FileMode.Append, "Append".Parse<FileMode>());
+    }
+
+    [TestMethod]
+    public void TestCheck()
+    {
+        string? nullString = null;
+        Assert.AreEqual(true, nullString.IsEmpty());
+        Assert.AreEqual(true, "".IsEmpty());
+        Assert.AreEqual(true, " ".IsEmpty());
+        Assert.AreEqual(false, " 123 ".IsEmpty());
+        Assert.AreEqual(true, "123".IsInteger());
+        Assert.AreEqual(false, "123.12".IsInteger());
+        Assert.AreEqual(false, "abc".IsInteger());
+
+        Assert.AreEqual(true, "me@you.de".IsEmail());
+        Assert.AreEqual(false, "me@you.de.".IsEmail());
+        Assert.AreEqual(false, "hello".IsEmail());
+
+        Assert.AreEqual(false, "abcde".Contains("bCd"));
+        Assert.AreEqual(true, "abcde".Contains("bCd", StringComparison.OrdinalIgnoreCase));
+        Assert.AreEqual(true, "abcde".ContainsIgnoreCase("bCd"));
+    }
+
+    [TestMethod]    
+    public void TestUnsorted()
+    {
+        Assert.AreEqual(3, "1,,2,3,4".SplitToType<int>(',')?.ToArray()[2]);
+        Assert.AreEqual(3, "1,,2;3,4;".SplitToType<int>(StringSplitOptions.RemoveEmptyEntries, ',', ';')?.ToArray()[2]);
+    }
+
+    [SupportedOSPlatform("windows")]
+    [TestMethod]
+    public void TestCryptWindows()
+    {
+        Assert.AreEqual("Hello World!", "Hello World!".EncryptRSA("MyKey")?.DecryptRSA("MyKey"));
     }
 }
